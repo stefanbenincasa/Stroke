3d31a297fc9e7f88b4ef79c9731547318c7e59b0
Stefan Benincasa <stefan.benin@gmail.com>
2020-05-17 16:08:39 +1000

    work on the 'drawFigure' function by splitting input charcters to array, checking for validity, and proceeding to stroke; basis of 'getStroke' still underway

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,84 @@
 import React, {useState, useEffect} from 'react';
-import {Container, Button, Form, FormGroup, Input} from 'reactstrap';
-import Canvas from './Canvas';
+import {Container, Button, Input, InputGroup} from 'reactstrap';
 import '../assets/App.css';
 
 export default function App() {
 
 	/// Hooks
-	const [keyMap, setKeyMap] = useState([
-		['a','b','c','d','e'],
-		['f','g','h','i','j'],
-		['k','l','m','n','o'],
-		['p','q','r','s','t'],
-		['u','v','w','x','y'],
-		['z']
-	]);
-	const [strokeWidths, setStrokeWidths] = useState([]); 
 
-	/// Functions
-	const drawFigure = function(e) {
-		e.preventDefault();
-		const userInput = document.getElementById('object').value; 
-		console.log(userInput); 
-	}
+	/// Variables
 
 	/// Render
   return (
     <div className="App">
 
-			<Form>
-				<FormGroup>
-					<Input 
-					id='object'
-					name='object'
-					type='text'
-					placeholder='Input object name'>
-					</Input>
-					<Button
-					onClick={drawFigure}>
-						Draw	
-					</Button>
-				</FormGroup>
-			</Form> 
+			<InputGroup>
+				<Input 
+				id='object'
+				name='object'
+				type='text'
+				placeholder='Input object name'>
+				</Input>
+				<Button
+				onClick={drawFigure}>
+					Draw	
+				</Button>
+			</InputGroup> 
 
-			<Canvas 
-			strokeWidths={strokeWidths} />
+			<canvas
+			id='canvas'
+			height='500'
+			width='500'>
+			</canvas> 
 
     </div>
   );
 }
 
-/// Helpers
+/// Functions
+function isLetter(letter) {
+	const charCode = letter.toUpperCase().charCodeAt(); 
+	console.log(charCode);
+	if ( (charCode >= 65) && (charCode <= 90) ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getStroke(character, canvas) {
+	
+	// Canvas limits
+	const limits = { x: canvas.width-25, y: canvas.height-25}
+	console.log(limits.x);
+	
+	// Stroke has a lineTo, x, and y, property
+
+}
+
+function drawFigure(e) {
+	e.preventDefault();
+
+	// Variables
+	const centerPoint = 250;
+	const canvas = document.getElementById('canvas');
+	const ctx = canvas.getContext('2d');
+
+	// Extract letters from 'object' input 
+	const userInput = document.getElementById('object').value; 
+	const characters = userInput.split('');
+	console.log(characters);
+
+	// Test for validity of each character, then make the approriate stroke
+	let stroke;
+	characters.forEach(character => {
+		if (isLetter(character)) {
+			getStroke(character, canvas);
+			ctx.moveTo(centerPoint, centerPoint);
+			ctx.lineTo(100,100);
+			ctx.stroke();
+		} else {
+			return;
+		}
+	}); 
+}
