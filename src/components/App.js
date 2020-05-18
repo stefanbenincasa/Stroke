8669a3f4d700158765a8fe2e22c9bd361bd00b83
Stefan Benincasa <stefan.benin@gmail.com>
2020-05-18 22:50:53 +1000

    implementation of algorithm to determine stroke vectors based on increasing value of key input; incomplete with refinement needed;

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,18 @@ import '../assets/App.css';
 
 export default function App() {
 
-	/// Hooks
-
-	/// Variables
-
 	/// Render
   return (
-
     <div className="App">
+			<h1>
+				Sentence Visualiser
+			</h1>
 			<InputGroup>
 				<Input 
 				id='object'
 				name='object'
 				type='text'
-				placeholder='Input object name'>
+				placeholder='Input sentence'>
 				</Input>
 				<Button
 				onClick={drawFigure}>
@@ -48,24 +46,23 @@ function isLetter(letter) {
 
 function setStroke(character, canvas) {
 
-	// Canvas limits; remember stroke is drawn from centerPoint aka 'halfway'
-	const limits = { x: canvas.width/2, y: canvas.height/2}
-
-	// Algorithm for the assignment of varying stroke, x, and y scalar 
-	// There should be 1/2 chance of negation of a co-ordinate (x,y)
-	let strokeWidth, x, y;
-	strokeWidth = Math.floor(Math.random() * Math.floor(15));
-	x = Math.floor(Math.random() * Math.floor(limits.x));
-	y = Math.floor(Math.random() * Math.floor(limits.y));
-	console.log(`${strokeWidth} ${x} ${y}`);
+	let charFloor, charValue, vectorBase, sqrtLimit;
+	let stroke = {vectors: []}
 	
-	// Stroke object has a strokeWidth, x, and y, property
-	const stroke = {
-		strokeWidth: strokeWidth,
-		x: x,
-		y: y
+	// Calculate difference of input character from lowest possible value
+	charFloor = 65;
+	charValue = character.toUpperCase().charCodeAt() - charFloor + 1;
+	console.log('CharValue : ' + charValue); 
+
+	// Determine the xy vectors for 'stroke', based on value of 'charValue'
+	for (let i = 1; i <= 2; i++) {
+		sqrtLimit = i === 1 ? Math.sqrt(canvas.height) : Math.sqrt(canvas.width);
+		vectorBase = charValue * (sqrtLimit/26);
+		stroke.vectors.push(Math.pow(vectorBase, 2)); 
 	}
 
+	// Return 'stroke'
+	console.log('Vectors : ' + stroke.vectors);
 	return stroke;
 }
 
@@ -73,9 +70,10 @@ function drawFigure(e) {
 	e.preventDefault();
 
 	// Variables
-	const centerPoint = 250;
 	const canvas = document.getElementById('canvas');
+	const centerPoint = {x: canvas.width/2, y: canvas.height/2}   
 	const ctx = canvas.getContext('2d');
+	console.log(centerPoint);
 
 	// Extract letters from 'object' input 
 	const userInput = document.getElementById('object').value; 
@@ -84,14 +82,28 @@ function drawFigure(e) {
 
 	// Test for validity of each character, then make the approriate stroke
 	let stroke;
+	ctx.strokeStyle = 'white';
 	characters.forEach(character => {
 		if (isLetter(character)) {
-			stroke = setStroke(character, canvas); // Use stroke props within
-			ctx.moveTo(centerPoint, centerPoint);
-			ctx.lineTo(100,100);
-			ctx.stroke();
+			stroke = setStroke(character, canvas); 
+			ctx.beginPath();
+			ctx.moveTo(centerPoint.x, centerPoint.y);
+			ctx.lineTo(stroke.vectors[0], stroke.vectors[1]);
+			ctx.stroke(); 
 		} else {
+			console.log('Invalid input. Not a letter.'); 
 			return;
 		}
 	}); 
 }
+
+/* 
+	const alphabet = [
+		'a','b','c','d','e',
+		'f','g','h','i','j',
+		'k','l','m','n','o',
+		'p','q','r','s','t',
+		'u','v','w','x','y',
+		'z'
+	]
+*/
